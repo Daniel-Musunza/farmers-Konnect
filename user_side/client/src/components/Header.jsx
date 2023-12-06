9777ed6bc3c7241312d007c1d68f3d73ee3cc844
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-12-06 20:02:46 +0300

    button2

--- user_side/client/src/components/Header.jsx
+++ user_side/client/src/components/Header.jsx
@@ -49,7 +49,7 @@ const setUser = () => {
           { account ? (
             <button><Link to="/post-land">Get Started</Link></button>
           ): (
-            <button> <div>connect to metamask</div></button>
+            <button style={{color: 'red'}}> connect to metamask</button>
           )}
             
           </div>
